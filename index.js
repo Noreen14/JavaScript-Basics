bd1f73d154a80648f3e7e92393561446380a50cb
Noreen Sulehry <noreenaziz803@gmail.com>
2020-10-01 10:30:08 -0600

    basics of javaScript

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("hello world");
+let x = 5;
+x = 10;
+console.log(x)
+let firstName = "Mary1";
+let FirstName = "Mary2";
+// console.log(firstName)
+// console.log(FirstName)
+const interestRate = 2.5;
+console.log(interestRate)
+console.log(typeof(interestRate))
+console.log(typeof(firstName))
+let y = null;
+console.log(typeof(y))
+function greet(name){
+    console.log("hello " + name)
+}
+greet("noreen")
+function test(){
+    return 5;
+}
+console.log(test())
+function square(toSquare){
+    return toSquare*toSquare;
+}
+console.log(square(6))
